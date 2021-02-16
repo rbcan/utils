8125626f3989b6a6c9a4d578732210dbebf51c45
Andrea Telatin <andrea.telatin@quadram.ac.uk>
2021-02-16 10:00:16 +0000

    allow execution form any path
    and create out dir if not found

--- kraken2-viz/render.R
+++ kraken2-viz/render.R
@@ -1,9 +1,16 @@
 library("rmarkdown")
 library("argparser")
 library("tools")
+# tidyverse ggdendro
+
+initial.options <- commandArgs(trailingOnly = FALSE)
+file.arg.name <- "--file="
+script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+script.basename <- dirname(script.name)
 
 # Parser
 par <- arg_parser("render kraken2 visualisation - report")
+
 # Arguments
 par <- add_argument(par, "--inbracken", help="merged bracken input file")
 par <- add_argument(par, "--inkraken", help="merged kraken input file")
@@ -18,10 +25,15 @@ if (pandoc_available())
 Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
 
 inbracken_file<-file_path_as_absolute(argv$inbracken)
-inbracken_file
 inkraken_file<-file_path_as_absolute(argv$inkraken)
-inkraken_file
-xxx<-file_path_as_absolute(argv$outdir)
-xxx
+cat("Kraken merged report:  ", inkraken_file, "\n")
+cat("Bracken merged report: ", inbracken_file, "\n")
 
-rmarkdown::render("kraken2-viz.R", params=list(inbracken=inbracken_file, inkraken=inkraken_file, topspecies=argv$topspecies, outdir=xxx), output_dir=argv$outdir)
+# Create if not existing (warn if exists)
+dir.create(file.path(argv$outdir))
+outputPath<-file_path_as_absolute(argv$outdir)
+cat("Output path:           ", outputPath, "\n")
+
+
+vizScript  <- file.path(script.basename, "kraken2-viz.R")
+rmarkdown::render(vizScript, params=list(inbracken=inbracken_file, inkraken=inkraken_file, topspecies=argv$topspecies, outdir=outputPath), output_dir=argv$outdir)
